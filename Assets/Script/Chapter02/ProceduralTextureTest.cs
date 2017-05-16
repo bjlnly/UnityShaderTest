efbe36eea8603fd6505e9bbc4c92a68322a4cf2f
刘洋 <117634430@qq.com>
2017-05-16 17:36:43 +0800

    代码生成材质球 模拟Photoshop色阶

---
+++ Assets/Script/Chapter02/ProceduralTextureTest.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+
+public class ProceduralTextureTest : MonoBehaviour {
+    #region 公有变量
+    public int widthHeight = 512;
+    public Texture2D GeneratedTexture2D;
+    #endregion
+
+    #region 私有变量
+    private Material currentMaterial;
+    private Vector2 centerPosition;
+    #endregion
+    // Use this for initialization
+    void Start () {
+        if (!currentMaterial)
+        {
+            currentMaterial = transform.GetComponent<Renderer>().sharedMaterial;
+            if (!currentMaterial)
+            {
+                StringBuilder debug = new StringBuilder();
+                Debug.LogWarning(debug.Append("找不到一个合适的材质：  ").Append(transform.name));
+            }
+        }
+
+        if (currentMaterial)
+        {
+            centerPosition = new Vector2(0.5f, 0.5f);
+            GeneratedTexture2D = GenerateParabola();
+
+            currentMaterial.SetTexture("_MainTex", GeneratedTexture2D);
+        }
+	}
+
+    private Texture2D GenerateParabola()
+    {
+        Texture2D proceduralTexture = new Texture2D(widthHeight, widthHeight);
+
+        Vector2 centerPixelPosition = centerPosition * widthHeight;
+
+        for (int i = 0; i < widthHeight; i++)
+        {
+            for (int j = 0; j < widthHeight; j++)
+            {
+                Vector2 currentPosition = new Vector2(i,j);
+                float pixelDistance = Vector2.Distance(currentPosition, centerPixelPosition) / (widthHeight * 0.5f);
+
+                pixelDistance = Mathf.Abs(1 - Mathf.Clamp(pixelDistance, 0f, 1f));
+
+                Color pixelColor = new Color(pixelDistance, pixelDistance, pixelDistance, 1.0f);
+                proceduralTexture.SetPixel(i, j, pixelColor);
+            }
+        }
+        proceduralTexture.Apply();
+        return proceduralTexture;
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/shader/Chapter02/PhotoShopLevel.shader
@@ -0,0 +1,58 @@
+﻿Shader "BJLN/CookBook/PhotoShopLevel" {
+	Properties {
+		_MainTex ("Albedo (RGB)", 2D) = "white" {}
+
+		_inBlack ("Input Back", Range(0, 255)) = 0
+		_inGame("Input Game", Range(0, 2)) = 1.61
+		_inWhite("Input White", Range(0, 255)) = 255
+
+		_outWhite("Output White", Range(0, 255)) = 255
+		_outBlack("Output Black", Range(0, 255)) = 0
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		LOD 200
+		
+		CGPROGRAM
+		// Physically based Standard lighting model, and enable shadows on all light types
+		#pragma surface surf Standard fullforwardshadows
+
+		// Use shader model 3.0 target, to get nicer looking lighting
+		#pragma target 3.0
+
+		sampler2D _MainTex;
+		float _inBlack;
+		float _inGame;
+		float _inWhite;
+		float _outWhite;
+		float _outBlack;
+
+		float GetPixelLevel(float pixelColor)
+		{
+			float pixelResult;
+			pixelResult = (pixelColor * 255.0);
+			pixelResult = max(0, pixelResult - _inBlack);
+
+			pixelResult = saturate(pow(pixelResult / (_inWhite - _inBlack), _inGame));
+
+			pixelResult = (pixelResult * (_outWhite - _outBlack) + _outBlack) / 255.0;
+			return pixelResult;
+		}
+		struct Input {
+			float2 uv_MainTex;
+		};
+
+		void surf (Input IN, inout SurfaceOutputStandard o) {
+			half4 c = tex2D(_MainTex, IN.uv_MainTex);
+
+			float outRPixel = GetPixelLevel(c.r);
+			float outGPixel = GetPixelLevel(c.g);
+			float outBPixel = GetPixelLevel(c.b);
+
+			o.Albedo = float3(outRPixel, outGPixel, outBPixel);
+			o.Alpha = c.a;
+		}
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
